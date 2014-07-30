f23d9822bbc1ea565b84cf49e8bcb9875531c021
Manuel Weber <manuel.weber@student.tugraz.at>
2014-07-31 01:52:36 +0200

    fixed a linkedhashtreemapbug

--- app/src/main/java/at/schuschu/android/rssilogger/GuessMyRoom.java
+++ app/src/main/java/at/schuschu/android/rssilogger/GuessMyRoom.java
@@ -130,7 +130,7 @@ public class GuessMyRoom extends Activity {
 
             acc_point_beliefs.put(rssi_signal.BSSID, new LinkedTreeMap<String, Float>());
             for (String room : room_probabilities.keySet()) {
-                acc_point_beliefs.get(rssi_signal).put(room, room_probabilities.get(room));
+                acc_point_beliefs.get(rssi_signal.BSSID).put(room, room_probabilities.get(room));
             }
             if (i > 3) {
                 break;
